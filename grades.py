d1efc8dfec6cd03ca7ccb3a551ad06f29d55d1f6
Aounathya K Rao <aounathyakrao@gmail.com>
2018-09-26 14:03:52 +0530

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,13 @@
+
+x=int(input("enter the value of x:"))
+if(x>90):
+	print("grade S")
+elif(x>70):
+	print("grade A")
+elif(x>60):
+	print("grade B")
+elif(x>50):
+	print("grade C")
+elif(x<40):
+	print("fail")		
+
---
+++ number.py
@@ -0,0 +1,5 @@
+x=int(input("enter a number:"))
+
+y=int(input("enter another number:"))
+
+print(x,"+",y,"=",x+y)
