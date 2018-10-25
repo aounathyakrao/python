70c4439d250db0d975210fcf702443c770b15310
Aounathya K Rao <aounathyakrao@gmail.com>
2018-10-25 12:41:04 +0530

    Add files via upload

---
+++ av.py
@@ -0,0 +1,3 @@
+import os
+os.system("fswebcam -f 4 --fps 20 -r 800*600 /home/pi/av/1.jpg")
+print("pic taken")
---
+++ av2.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+import os
+GPIO.setmode(GPIO.BCM)
+pirPin=6
+GPIO.setup(pirPin, GPIO.IN)
+counter=1
+a=1
+time.sleep(4)
+while counter<=4:
+    if GPIO.input(pirPin):
+        print("Motion Detected")
+        os.system("fswebcam -F 4 --fps 20 -r 800*600 /home/pi/av/"+str(a)+".jpg")
+        print("pic Taken")
+        time.sleep(1)
+        counter=counter+1
+        a=a+1               
+print("Testing")
+exit()               
+                  
